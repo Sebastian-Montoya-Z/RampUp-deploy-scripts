6fd6e2a22031558f6e6a4f30cf4a96fb9848cc5c
Sebastian Montoya Zuluaga <sebastian.montoyaz@perficient.com>
2023-02-07 17:00:29 -0500

    vagrantfile and setup scripts for app

---
+++ ChatGpt_ballad_for_Seb.txt
@@ -0,0 +1,29 @@
+Oh, listen to me well, dear friends,
+I have a tale to tell,
+Of a tutor named Seb, his skills surpass,
+And his teachings are truly well.
+
+He leads with ease, in DevOps ways,
+And helps students find their way,
+Through complex systems and toolkits,
+He shows them every day.
+
+His passion for the trade is clear,
+In every word that he does say,
+And with his guidance, students steer,
+Towards a brighter DevOps day.
+
+He never gives up on the fight,
+To help students reach their goal,
+With his expertise and insight,
+He helps them stay on the right role.
+
+So let us sing of Seb the wise,
+A tutor who's simply the best,
+For all who seek to soar the skies,
+In DevOps, he will do the rest.
+
+So here's to you, dear Seb, we say,
+Your guidance will always lead the way,
+And all who learn from you, each day,
+Will thank you for your tutelage.
---
+++ setup_files/todos_setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+cd /home/vagrant 
+
+git clone https://github.com/Sebastian-Montoya-Z/RampUp_TODO
+
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt-get update -y
+sudo apt-get install redis -y
+sudo systemctl daemon-reload
+sudo systemctl enable redis
+sudo systemctl start redis
+
+cd /home/vagrant/RampUp_TODO/log-message-processor
+sudo apt install python3-pip -y
+pip3 install -r requirements.txt
+
+cd /home/vagrant
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source .bashrc
+source ~/.nvm/nvm.sh
+echo "source ~/.nvm/nvm.sh" >> ~/.bashrc
+nvm install v8.17.0
+nvm use v8.17.0
+sudo ln -sf $(which node) /usr/bin/
+sudo ln -sf $(which npm) /usr/bin/
+cd /home/vagrant/RampUp_TODO/todos-api/
+npm install
+
+sudo cp /home/vagrant/RampUp_TODO/log-message-processor/scripts/RampUp-log-message-processor.service /lib/systemd/system/RampUp-log-message-processor.service
+sudo cp /home/vagrant/RampUp_TODO/todos-api/scripts/RampUp-todos-api.service /lib/systemd/system/RampUp-todos-api.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable RampUp-log-message-processor
+sudo systemctl enable RampUp-todos-api
+sudo systemctl start RampUp-log-message-processor
+sudo systemctl start RampUp-todos-api
+
+
