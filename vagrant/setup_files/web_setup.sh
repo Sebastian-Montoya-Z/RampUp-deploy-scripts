946280cbf8cdbd49e59f35cba7faec265975bb6d
Sebastian Montoya Zuluaga <sebastian.montoyaz@perficient.com>
2023-02-15 14:36:37 -0500

    idempotency

--- vagrant/setup_files/web_setup.sh
+++ vagrant/setup_files/web_setup.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
-sudo apt-get update
+sudo chmod 744 upgrade.sh
+sudo ./upgrade.sh
 
 cd /home/vagrant 
 
 git clone https://github.com/Sebastian-Montoya-Z/RampUp_frontend
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-source .bashrc
-source ~/.nvm/nvm.sh
-echo "source ~/.nvm/nvm.sh" >> ~/.bashrc
-nvm install v8.17.0
-nvm use v8.17.0
-sudo rm -f /usr/bin/node
-sudo rm -f /usr/bin/npm
-sudo ln -sf $(which node) /usr/bin/
-sudo ln -sf $(which npm) /usr/bin/
+sudo chmod 744 node_install.sh 
+sudo ./node_install.sh
 cd /home/vagrant/RampUp_frontend/frontend
 npm install
 npm run build
